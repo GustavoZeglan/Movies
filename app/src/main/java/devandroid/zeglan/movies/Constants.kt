a0731ae47f04e03b69a5f30b523ee26e6c5e400e
GustavoZeglan <89813478+GustavoZeglan@users.noreply.github.com>
2023-10-28 00:42:04 -0300

    Update Constants.kt
    remove my tokens kkk

--- app/src/main/java/devandroid/zeglan/movies/Constants.kt
+++ app/src/main/java/devandroid/zeglan/movies/Constants.kt
@@ -3,8 +3,8 @@ package devandroid.zeglan.movies
 class Constants {
 
     companion object USER {
-        val TOKEN: String = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZDYzNDI2MGZjOTUyZWE3ZTdjNTI5ODhiMjhjN2JhNCIsInN1YiI6IjYxMzU3YTAyMGI3MzE2MDA4YTYyODhlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vxct0T55ON_qPgszzY751bwo2AqcX0HELZERKgDQAlE"
-        val ACCOUNTID: Int = 11056694
+        val TOKEN: String = ""
+        val ACCOUNTID: Int = 0
     }
 
 }
